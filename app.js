b6ca79aa9d278697524a6f3f9c7fa92329f1c756
cris <cris@contactweb.com>
2021-02-17 23:38:34 +0100

    saludo js agregado

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('Hola a todos!!');
+
+function saludo(){
+	console.log('Esto es el metodo saludo()');
+	alert("Hola GitHub");
+}
---
+++ javascripts/application.js
@@ -0,0 +1,8 @@
+//
+console.log('Estamos en application.js');
+
+function saludar(){
+	console.log('Esto es una prueba');
+	console.log('Estamos en el método saludar()');
+	alert("Ya estamos aquí!!!");
+}
